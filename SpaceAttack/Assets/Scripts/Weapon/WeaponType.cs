b573c0ee21f5a93e52f121e6c9cdf05416f51af2
Taehyeon88 <Taehyeon5376@naver.com>
2025-07-07 23:05:24 +0900

    [Pro upd] 사망 애니메이션 추가 및 무기 부모클래스 추상 클래스로 변경
    추상 클래스로 변경 이유: 사용성 향상

--- SpaceAttack/Assets/Scripts/Weapon/WeaponType.cs
+++ SpaceAttack/Assets/Scripts/Weapon/WeaponType.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponType : MonoBehaviour, IAttack, ICheckAttack
+public abstract class WeaponType : MonoBehaviour, IAttack, ICheckAttack
 {
     //임시로 플레이어에 할당
     public Animator attackAnimator { get; set; }            //공격 내부에서 쓸 애니메이터
@@ -15,13 +15,13 @@ public class WeaponType : MonoBehaviour, IAttack, ICheckAttack
     public bool isAttackMoving { get; protected set; }      //공격 이동 여부
     public Timer w_AttackTimer { get; protected set; }      //다음 공격 대기 타이머
 
-    public virtual void CheckAttack(Vector2 currentPos) { }
+    public abstract void CheckAttack(Vector2 currentPos);
 
-    public virtual void Attack() { }
+    public abstract void Attack();
 
     //Start 대신으로 임시 지정
-    public virtual void OnEnable() { }
+    public abstract void OnEnable();
     //Update 대신으로 임시 지정
-    public virtual void UpdateInfo() { }
+    public abstract void UpdateInfo();
 
 }
