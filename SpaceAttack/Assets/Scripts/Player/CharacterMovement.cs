1b6c0cac6901d9f2014793eab83a0507e0330b87
Taehyeon88 <Taehyeon5376@naver.com>
2025-08-19 21:40:09 +0900

    [Pro Upd] 잡몹 이미지 기울기 설정 초기화

--- SpaceAttack/Assets/Scripts/Player/CharacterMovement.cs
+++ SpaceAttack/Assets/Scripts/Player/CharacterMovement.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CharacterMovement : MonoBehaviour
 {
