3a005058a20aff032a494d07dd69505508ff8992
Taehyeon88 <Taehyeon5376@naver.com>
2025-10-30 08:20:05 +0900

    [Pro Fix] 몬스터 오디오믹서 추가 및 플레이어 대쉬 쿨타임, 거리 수치 변경

--- SpaceAttack/Assets/Scripts/Player/PlayerTimeSystem.cs
+++ SpaceAttack/Assets/Scripts/Player/PlayerTimeSystem.cs
@@ -8,7 +8,7 @@ public class PlayerTimeSystem : MonoBehaviour
     public static float m_DashTime = 0.2f;       //대쉬 시간
     public static float m_stunTime = 0.3f;       //스턴 시간
     public static float m_invincibilityTime = 0.3f;  //대쉬이후 무적 시간
-    public static float c_DashTime = 3f;              //대쉬 쿨타임
+    public static float c_DashTime = 0.8f;              //대쉬 쿨타임
 
     public static Timer stunTimer;                       //피격 이후 스턴(무적)시간
     public static Timer invincibilityTimer;              //대쉬 이후 무적시간
